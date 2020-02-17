fd028ad5e34091cf7a2d5fce7986d33d321204c9
pradel <pradel.leo@gmail.com>
2020-02-17 11:36:06 +0100

    Setup mongoose

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require("apollo-server");
+const mongoose = require("mongoose");
+const { Mongo } = require("@accounts/mongo");
+
+// We connect mongoose to our local mongodb database
+mongoose.connect("mongodb://localhost:27017/accounts-js-server", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// We tell accounts-js to use the mongo connection
+const accountsMongo = new Mongo(mongoose.connection);
 
 const typeDefs = gql`
   type Query {
@@ -9,8 +20,8 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    sensitiveInformation: () => 'Sensitive info',
-  },
+    sensitiveInformation: () => "Sensitive info"
+  }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
