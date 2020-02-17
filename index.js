40c6a753d0b582f802865af712df248bee255e36
pradel <pradel.leo@gmail.com>
2020-02-17 11:38:25 +0100

    Setup accounts-js server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { ApolloServer, gql } = require("apollo-server");
 const mongoose = require("mongoose");
 const { Mongo } = require("@accounts/mongo");
+const { AccountsServer } = require("@accounts/server");
+const { AccountsPassword } = require("@accounts/password");
 
 // We connect mongoose to our local mongodb database
 mongoose.connect("mongodb://localhost:27017/accounts-js-server", {
@@ -10,6 +12,23 @@ mongoose.connect("mongodb://localhost:27017/accounts-js-server", {
 
 // We tell accounts-js to use the mongo connection
 const accountsMongo = new Mongo(mongoose.connection);
+
+const accountsPassword = new AccountsPassword({
+  // You can customise the behavior of the password service by providing some options
+});
+
+const accountsServer = new AccountsServer(
+  {
+    // We link the mongo adapter to the server
+    db: accountsMongo,
+    // Replace this value with a strong random secret
+    tokenSecret: "my-super-random-secret"
+  },
+  {
+    // We pass a list of services to the server, in this example we just use the password service
+    password: accountsPassword
+  }
+);
 
 const typeDefs = gql`
   type Query {
