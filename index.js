5d94eefd75715fd988d47eccf0cce5b9e14c2cc9
pradel <pradel.leo@gmail.com>
2021-02-01 12:07:21 +0100

    update example to make it work with accounts-js 0.30+

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ApolloServer, gql, makeExecutableSchema } = require("apollo-server");
-const { mergeTypeDefs, mergeResolvers } = require("@graphql-toolkit/schema-merging");
+const { mergeTypeDefs, mergeResolvers } = require("@graphql-tools/merge");
 const { AccountsModule } = require("@accounts/graphql-api");
 const mongoose = require("mongoose");
 const { Mongo } = require("@accounts/mongo");
